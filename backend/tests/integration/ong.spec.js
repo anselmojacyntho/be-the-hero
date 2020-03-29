f890f8349ba532f5088681fb17e61bb796e59e67
Anselmo Barbosa <anselmojacyntho@gmail.com>
2020-03-29 17:45:00 -0300

    Test do backend

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+
+    beforeEach(async() => {
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll(async() => {
+        await connection.destroy();
+    });
+
+    it('should be able to create a new ong', async() => {
+
+        const response = await request(app).post('/ongs').send({
+            name: 'ONG',
+            email: "teste@gmail.com",
+            whatsapp: "11988776644",
+            city: "mongagua",
+            uf: "SP"
+        });
+
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+    });
+});
